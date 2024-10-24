5ced0c2fc1ba136da211985214677c2e31d24bee
dolurombi <olurombid@gmail.com>
2024-10-24 15:39:55 +0100

    login script

---
+++ cypress/e2e/logintest.cy.js
@@ -0,0 +1,9 @@
+describe("login",()=>{
+    it("successfulLogin",()=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        cy.get("input[placeholder='Username']").type("Admin",{delay:200});
+        cy.get("input[placeholder='Password']").type("admin123");
+        cy.get("button[type='submit']").click();
+        cy.title().should("equal","OrangeHRM")
+    });
+});
